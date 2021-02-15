6591b33c5b9b246dca5efc034a9fbfe2011b395e
verzelea <50270157+verzelea@users.noreply.github.com>
2021-02-15 19:11:54 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import fastify from 'fastify';
+// see axios doc on how to use it
+import axios from 'axios';
+
+const app = fastify({ logger: true });
+
+app.get('/', async (req, res) => {
+  return {
+    message: `Welcome to Node Babel with ${
+      req.body?.testValue ?? 'no testValue'
+    }`,
+  };
+});
+
+// Run the server!
+const start = async () => {
+  try {
+    await app.listen(5000);
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+start();
